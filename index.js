82295ba744c43ac9e9818d27a267641882379a36
hmchamberl42 <hmchamberl42@students.tntech.edu>
2019-12-05 22:14:24 -0600

    more changes

--- index.js
+++ index.js
@@ -2,29 +2,37 @@
 
 const projects = [
     {
-        imgsrc: 'img/hudson-img.jpg',
-        title: 'Hello',
-        date: 'date',
-        description: 'desc'
+        imgsrc: 'img/school-in-the-cloud.png',
+        title: 'School in the Cloud',
+        titleLink: 'https://github.com/School-in-the-Cloud/UI',
+        date: '11/20/2019',
+        languages: 'HTML, CSS',
+        description: 'Online service for students wanting to learn from volunteers. The website is responsive and backend setup to support users.'
     },
     {
-        imgsrc: 'img/hudson-img.jpg',
-        title: 'Hello',
-        date: 'date',
-        description: 'desc'
+        imgsrc: 'img/school-in-the-cloud.png',
+        title: 'School in the Cloud',
+        titleLink: 'https://github.com/School-in-the-Cloud/UI',
+        date: '11/20/2019',
+        languages: 'HTML, CSS',
+        description: 'Online service for students wanting to learn from volunteers. The website is responsive and backend setup to support users.'
     },
     {
-        imgsrc: 'img/hudson-img.jpg',
-        title: 'Hello',
-        date: 'date',
-        description: 'desc'
+        imgsrc: 'img/school-in-the-cloud.png',
+        title: 'School in the Cloud',
+        titleLink: 'https://github.com/School-in-the-Cloud/UI',
+        date: '11/20/2019',
+        languages: 'HTML, CSS',
+        description: 'Online service for students wanting to learn from volunteers. The website is responsive and backend setup to support users.'
     },
     {
-        imgsrc: 'img/hudson-img.jpg',
-        title: 'Hello',
-        date: 'date',
-        description: 'desc'
-    }
+        imgsrc: 'img/school-in-the-cloud.png',
+        title: 'School in the Cloud',
+        titleLink: 'https://github.com/School-in-the-Cloud/UI',
+        date: '11/20/2019',
+        languages: 'HTML, CSS',
+        description: 'Online service for students wanting to learn from volunteers. The website is responsive and backend setup to support users.'
+    },
 ];
 
 const projectsEl = document.querySelector('#projects');
@@ -37,11 +45,16 @@ projects.forEach( obj => {
 function createProject(obj) {
     // <div class="project">
     //     <div class="content-img">
-    //         <img src="src"/>
+    //         <a href="">
+    //             <img src="src"/>
+    //         </a>
     //     </div>
     //     <div class="project-info">
-    //         <h3>Title</h3>
+    //         <a href="">
+    //             <h3>Title</h3>
+    //         </a>
     //         <h5>Date</h5>
+    //         <p>languages</p>
     //         <p>description</p>
     //     </div>
     // </div>
@@ -53,21 +66,35 @@ function createProject(obj) {
     imgDiv.classList.add('content-img');
     project.appendChild(imgDiv);
 
+    const imgAnchor = document.createElement('a');
+    imgAnchor.href = obj.titleLink;
+    imgAnchor.target = "_blank";
+    imgDiv.appendChild(imgAnchor);
+
     const img = document.createElement('img');
     img.src = obj.imgsrc;
-    imgDiv.appendChild(img);
+    imgAnchor.appendChild(img);
 
     const projectInfo = document.createElement('div');
     projectInfo.classList.add('project-info');
     project.appendChild(projectInfo);
 
+    const titleAnchor = document.createElement('a');
+    titleAnchor.href = obj.titleLink;
+    titleAnchor.target = "_blank";
+    projectInfo.appendChild(titleAnchor);
+    
     const title = document.createElement('h3');
     title.textContent = obj.title;
-    projectInfo.appendChild(title);
+    titleAnchor.appendChild(title);
 
     const date = document.createElement('h5');
     date.textContent = obj.date;
     projectInfo.appendChild(date);
+
+    const languages = document.createElement('p');
+    languages.textContent = obj.languages;
+    projectInfo.appendChild(languages);
 
     const desc = document.createElement('p');
     desc.textContent = obj.description;
