c680c672b38bb1cac95d41f846634b865f3142b8
Hudson Chamberlain <hudson.m.chamberlain@gmail.com>
2020-04-16 08:49:45 -0500

    footer and copyright symbol setup

---
+++ scripts/components/footer.js
@@ -0,0 +1,76 @@
+// create the footer
+console.log('adding footer');
+document.querySelector('.container').appendChild( createFooter() );
+
+function createFooter() {
+    /*
+        Skeleton:
+
+        <footer>
+
+            <div className="top-footer">
+                <div/> // empty for spacing
+                <div> // for name
+                    Hudson Chamberlain
+                </div>
+                <div className="links-footer">
+                    <div className="link">Home</div>
+                    <div className="link">Projects</div>
+                    <div className="link">Articles</div>
+                </div>
+            </div>
+
+            <div className="bottom-footer">
+                <p className="copyright">&copy Hudson Chamberlain 2020</p>
+            </div>
+
+        </footer>
+
+    */
+    
+    const footer = document.createElement('footer');
+
+    // create top half
+    const topFooter = document.createElement('div');
+    topFooter.classList.add('top-footer');
+
+    // empty div
+    topFooter.appendChild( document.createElement('div') ); // empty div
+    
+    // name div
+    const name = document.createElement('div');
+    name.textContent = "Hudson Chamberlain";
+    topFooter.appendChild(name);
+
+    // link div
+    const links = document.createElement('div');
+    links.classList.add('links-footer');
+    linksText = ["Home", "Projects", "Articles"];
+    linksText.forEach( curr => {
+        const link = document.createElement('div');
+        link.textContent = curr;
+        link.addEventListener('click', (curr) => {
+            console.log('clicked footer: ', curr);
+        })
+        links.appendChild(link);
+    })
+    topFooter.appendChild(links);
+
+    // add top footer
+    footer.appendChild(topFooter);
+
+    //bottom footer
+    const bottomFooter = document.createElement('div');
+    bottomFooter.classList.add('bottom-footer');
+    
+    const copy = document.createElement('p');
+    copy.classList.add("copyright");
+    copy.innerHTML = "&copy; Hudson Chamberlain 2020";
+    bottomFooter.appendChild(copy);
+
+    // add bottom footer
+    footer.appendChild(bottomFooter);
+
+
+    return footer;
+}
--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,16 +1,22 @@
 // here we will create all of the necessary components
 
 // add the header
-let headerScript = document.createElement('script');
+const headerScript = document.createElement('script');
 headerScript.src = './scripts/components/header.js';
 document.head.appendChild(headerScript);
 
 // add the straight script
-let straightScript = document.createElement('script');
+const straightScript = document.createElement('script');
 straightScript.src = './scripts/components/straight_to_the_point.js';
 document.head.appendChild(straightScript);
 
 // add the questions
-let questionsScript = document.createElement('script');
+const questionsScript = document.createElement('script');
 questionsScript.src = './scripts/components/questions/main_script.js';
 document.head.appendChild(questionsScript);
+
+// add the footer
+console.log('adding footer script');
+const footerScript = document.createElement('script');
+footerScript.src = "./scripts/components/footer.js";
+document.head.appendChild(footerScript);
