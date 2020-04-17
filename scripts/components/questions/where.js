fdb4c9a9ad33ef32341b95c219754b7ecf093093
Hudson Chamberlain <hudson.m.chamberlain@gmail.com>
2020-04-17 04:12:09 -0500

    where done

---
+++ scripts/components/questions/where.js
@@ -0,0 +1,7 @@
+// grab when and change the text
+
+const answerWhere = document.querySelectorAll('.main-question-Where .main-question-answer p')[0];
+
+let whereText = "Remote and travel for contracts. Relocation for long term positions.";
+
+answerWhere.textContent = whereText;
